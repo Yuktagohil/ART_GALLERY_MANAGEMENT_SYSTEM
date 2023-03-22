2822977178ca50cccbeb866b13d3609a0065132b
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-03-22 13:51:04 -0400

    Add upcoming exhibition view

---
+++ USERGRANT.sql
@@ -0,0 +1,11 @@
+Drop user urbandusters;
+
+Create user urbandusters identified by DmddProject3;
+
+grant connect to urbandusters;
+
+grant resource to urbandusters;
+
+alter user urbandusters quota 20M on data;
+
+grant create view to urbandusters;
