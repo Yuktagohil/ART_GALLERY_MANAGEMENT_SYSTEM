1afb65ad9b366c5faa0bb5bc307d6eacf6550db4
Marturu3 <114604268+Marturu3@users.noreply.github.com>
2023-03-23 00:22:49 -0400

    Add files via upload

---
+++ ordertables (1).sql
@@ -0,0 +1,25 @@
+
+CREATE TABLE ORDER (
+    OrderID INTEGER(100) NOT NULL PRIMARY KEY,
+    UserID INTEGER(100) NOT NULL,
+    ShipperID INTEGER(100) NOT NULL,
+    TransactionID INTEGER(100) NOT NULL,
+    TransactionMethod VARCHAR(45) NOT NULL,
+    TransactionStatus VARCHAR(45) NOT NULL,
+    OrderStatus VARCHAR(45) NOT NULL,
+    ShippingStatus VARCHAR(45) NOT NULL,
+    ShippingAddress VARCHAR(45) NOT NULL,
+    OrderTimeDate TIMESTAMP NOT NULL,
+    TotalAmount INT NOT NULL,
+    CONSTRAINT fk1 FOREIGN KEY (UserID)
+        REFERENCES users (UserID),
+    CONSTRAINT fk2 FOREIGN KEY (ShipperID)
+        REFERENCES shipper (ShipperID)
+);
+
+CREATE TABLE ORDER_ITEMS (
+    OrderItemsID INTEGER(100) PRIMARY KEY,
+    OrderID INTEGER(100) NOT NULL,
+    FOREIGN KEY (OrderID)
+        REFERENCES orders (OrderID)
+);
