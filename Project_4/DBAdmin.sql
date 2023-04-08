77e44bb41f9ffcd778deb13d1a79b7a7053d31cf
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-04-08 13:05:05 -0400

    Project 4 new folder

--- Project_4/DBAdmin.sql
+++ Project_4/DBAdmin.sql
@@ -1,15 +1,6 @@
 SET SERVEROUTPUT ON;
 
 BEGIN
-EXECUTE IMMEDIATE 'DROP ROLE admin CASCADE';
-dbms_output.put_line('Objects dropped');
-EXCEPTION
-WHEN OTHERS
-THEN dbms_output.put_line('Objects not found');
-END;
-/ 
-
-BEGIN
 EXECUTE IMMEDIATE 'DROP ROLE artist CASCADE';
 dbms_output.put_line('Objects dropped');
 EXCEPTION
@@ -27,20 +18,10 @@ THEN dbms_output.put_line('Objects not found');
 END;
 / 
 
-CREATE ROLE admin IDENTIFIED BY April2023; 
-CREATE ROLE artist IDENTIFIED BY April2023; 
-CREATE ROLE customer IDENTIFIED BY April2023;
+CREATE ROLE artist IDENTIFIED BY artist004; 
+CREATE ROLE customer IDENTIFIED BY customer004;
 
-GRANT CREATE SESSION TO admin, artist, customer;
-
-BEGIN
-EXECUTE IMMEDIATE 'DROP USER admn CASCADE';
-dbms_output.put_line('Objects dropped');
-EXCEPTION
-WHEN OTHERS
-THEN dbms_output.put_line('Objects not found');
-END;
-/
+GRANT CREATE SESSION TO artist, customer;
 
 BEGIN
 EXECUTE IMMEDIATE 'DROP USER arst CASCADE';
@@ -60,14 +41,11 @@ THEN dbms_output.put_line('Objects not found');
 END;
 / 
 
-CREATE USER admn IDENTIFIED BY admin004; 
-GRANT CREATE SESSION TO admn;
-GRANT admin TO admn;
-
-CREATE USER arst IDENTIFIED BY artist004; 
+CREATE USER arst IDENTIFIED BY November2022; 
 GRANT CREATE SESSION TO arst;
-GRANT artist TO arst;
+grant artist TO arst;
 
-CREATE USER cust IDENTIFIED BY customer004; 
+CREATE USER cust IDENTIFIED BY November2022; 
 GRANT CREATE SESSION TO cust;
 GRANT customer TO cust;
+
