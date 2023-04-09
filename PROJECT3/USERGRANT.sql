d75ecddb45214fe68308851e1c5149b6f3c53150
yuktaba.gohil <yuktaba.gohil@letsnurture.co.in>
2023-04-09 11:49:06 -0400

    Insert constraints

--- PROJECT3/USERGRANT.sql
+++ PROJECT3/USERGRANT.sql
@@ -18,23 +18,3 @@ grant resource to urbandusters;
 alter user urbandusters quota 100M on data;
 
 grant create view to urbandusters;
-
-
-BEGIN
-EXECUTE IMMEDIATE 'DROP USER DBAdmin CASCADE';
-dbms_output.put_line('Objects dropped');
-EXCEPTION
-WHEN OTHERS
-THEN dbms_output.put_line('Objects not found');
-END;
-/ 
-
-Create user DBAdmin identified by DmddProject4;
-
-grant connect to DBAdmin;
-
-grant resource to DBAdmin;
-
-alter user DBAdmin quota 500M on data;
-
-grant create view to DBAdmin;
