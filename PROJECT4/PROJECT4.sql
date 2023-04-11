3350521c2a71cd281a2e3b11c96ee1ca1c6309bb
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-04-11 13:50:19 -0400

    Procedure to manage users

--- PROJECT4/PROJECT4.sql
+++ PROJECT4/PROJECT4.sql
@@ -161,6 +161,24 @@ WHEN OTHERS
 THEN dbms_output.put_line('Objects not found');
 END;
 /  
+
+BEGIN
+EXECUTE IMMEDIATE 'DROP FUNCTION check_online_exhibition_status';
+dbms_output.put_line('Objects dropped');
+EXCEPTION
+WHEN OTHERS
+THEN dbms_output.put_line('Objects not found');
+END;
+/  
+
+BEGIN
+EXECUTE IMMEDIATE 'DROP PROCEDURE manage_users';
+dbms_output.put_line('Objects dropped');
+EXCEPTION
+WHEN OTHERS
+THEN dbms_output.put_line('Objects not found');
+END;
+/  
   
 Create table USER_ROLE(
 RoleID integer NOT NULL PRIMARY KEY,
@@ -670,67 +688,6 @@ GRANT SELECT, INSERT ON ORDER_ITEMS TO CUSTOMER;
 GRANT SELECT ON ART_CATEGORY TO CUSTOMER;
 GRANT SELECT ON ARTWORK TO CUSTOMER;
 
-
---function to manage users
-CREATE OR REPLACE FUNCTION manage_users1 (
-    p_user_id IN NUMBER DEFAULT NULL,
-    p_role_id IN NUMBER,
-    p_email_id IN VARCHAR2,
-    p_username IN VARCHAR2,
-    p_password IN VARCHAR2,
-    p_firstname IN VARCHAR2,
-    p_lastname IN VARCHAR2,
-    p_speciality IN VARCHAR2 DEFAULT NULL,
-    p_nationality IN VARCHAR2 DEFAULT NULL,
-    p_contact_number IN VARCHAR2 DEFAULT NULL,
-    p_address_line1 IN VARCHAR2 DEFAULT NULL,
-    p_address_line2 IN VARCHAR2 DEFAULT NULL,
-    p_city IN VARCHAR2 DEFAULT NULL,
-    p_state IN VARCHAR2 DEFAULT NULL,
-    p_country IN VARCHAR2 DEFAULT NULL,
-    p_zipcode IN NUMBER DEFAULT NULL,
-    p_action IN VARCHAR2
-)
-RETURN VARCHAR2
-IS
-    v_user_id NUMBER(5);
-    v_msg VARCHAR2(200);
-BEGIN
-    IF (p_action = 'ADD') THEN
-            IF p_user_id IS NULL THEN
-            SELECT MAX(UserID) + 1 INTO v_user_id FROM USERS;
-        ELSE
-            v_user_id := p_user_id;
-        END IF;
-        
-        INSERT INTO USERS (UserID, RoleID, EmailID, UserName, Password, FirstName, LastName, Speciality, Nationality)
-        VALUES (users_seq.NEXTVAL, p_role_id, p_email_id, p_username, p_password, p_firstname, p_lastname, p_speciality, p_nationality);
-
-        INSERT INTO CONTACT (ContactID, UserID, ContactNumber, AddressLine1, AddressLine2, City, State, Country, ZipCode)
-        VALUES (contact_seq.NEXTVAL, users_seq.CURRVAL, p_contact_number, p_address_line1, p_address_line2, p_city, p_state, p_country, p_zipcode);
-
-        v_msg := 'User added successfully';
-    ELSIF (p_action = 'UPDATE') THEN
-        UPDATE USERS SET RoleID = p_role_id, EmailID = p_email_id, UserName = p_username, Password = p_password, FirstName = p_firstname, LastName = p_lastname, Speciality = p_speciality, Nationality = p_nationality
-        WHERE UserID = p_user_id;
-
-        UPDATE CONTACT SET ContactNumber = p_contact_number, AddressLine1 = p_address_line1, AddressLine2 = p_address_line2, City = p_city, State = p_state, Country = p_country, ZipCode = p_zipcode
-        WHERE UserID = p_user_id;
-
-        v_msg := 'User updated successfully';
-    ELSIF (p_action = 'DELETE') THEN
-        DELETE FROM USERS WHERE UserID = p_user_id;
-        DELETE FROM CONTACT WHERE UserID = p_user_id;
-
-        v_msg := 'User deleted successfully';
-    ELSE
-        v_msg := 'Invalid action';
-    END IF;
-
-    RETURN v_msg;
-END;
-/
-
 --function to purchase artwork
 CREATE OR REPLACE FUNCTION purchase_artwork(p_UserID IN NUMBER, p_ArtworkID IN NUMBER)
 RETURN VARCHAR
@@ -817,3 +774,149 @@ WHERE ArtworkID = p_artwork_id;
 END IF;
 END;
 /
+
+--procedure to manage users
+CREATE OR REPLACE PROCEDURE manage_users (
+    p_user_id IN USERS.USERID%TYPE,
+    p_role_id IN USERS.ROLEID%TYPE DEFAULT NULL,
+    p_email_id IN USERS.EMAILID%TYPE DEFAULT NULL,
+    p_user_name IN USERS.USERNAME%TYPE DEFAULT NULL,
+    p_password IN USERS.PASSWORD%TYPE DEFAULT NULL,
+    p_first_name IN USERS.FIRSTNAME%TYPE DEFAULT NULL,
+    p_last_name IN USERS.LastName%TYPE DEFAULT NULL,
+    p_speciality IN USERS.SPECIALITY%TYPE DEFAULT NULL,
+    p_nationality IN USERS.NATIONALITY%TYPE DEFAULT NULL,
+    p_contact_id IN CONTACT.CONTACTID%TYPE DEFAULT NULL,
+    p_contact_number IN CONTACT.CONTACTNUMBER%TYPE DEFAULT NULL,
+    p_address_line1 IN CONTACT.ADDRESSLINE1%TYPE DEFAULT NULL,
+    p_address_line2 IN CONTACT.ADDRESSLINE2%TYPE DEFAULT NULL,
+    p_city IN CONTACT.CITY%TYPE DEFAULT NULL,
+    p_state IN CONTACT.STATE%TYPE DEFAULT NULL,
+    p_country IN CONTACT.COUNTRY%TYPE DEFAULT NULL,
+    p_zipcode IN CONTACT.ZIPCODE%TYPE DEFAULT NULL,
+    p_action IN VARCHAR2,
+    p_msg OUT VARCHAR2
+)
+IS
+    a_user_id NUMBER(5);
+    v_user_id NUMBER(5);
+    v_email_exists NUMBER(1) := 0;
+BEGIN
+    IF (p_action = 'ADD') THEN
+        -- Check for duplicate email
+        SELECT COUNT(*) INTO v_email_exists FROM USERS WHERE EmailID = p_email_id;
+        IF v_email_exists > 0 THEN
+            p_msg := 'Email already exists';
+            RETURN;
+        END IF;
+        IF LENGTH(p_contact_number) != 10 THEN
+            p_msg := 'Mobile Number should be 10 digits';
+            RETURN;
+        END IF;
+        IF p_role_id IS NULL THEN
+            p_msg := 'Role cannot be null';
+            RETURN;
+        END IF;
+        IF p_email_id IS NULL THEN
+            p_msg := 'Email id cannot be null';
+            RETURN;
+        END IF;
+        IF p_user_name IS NULL THEN
+            p_msg := 'Username cannot be null';
+            RETURN;
+        END IF;
+        IF p_password IS NULL THEN
+            p_msg := 'Password cannot be null';
+            RETURN;
+        END IF;
+        IF p_first_name IS NULL THEN
+            p_msg := 'Firstname cannot be null';
+            RETURN;
+        END IF;
+        IF p_last_name IS NULL THEN
+            p_msg := 'Lastname cannot be null';
+            RETURN;
+        END IF;
+        IF p_contact_id IS NOT NULL THEN
+            p_msg := 'Contact id should be null for add action';
+            RETURN;
+        END IF;
+        IF p_contact_number IS NULL THEN
+            p_msg := 'Mobile Number cannot be null';
+            RETURN;
+        END IF;
+        IF p_address_line1 IS NULL THEN
+            p_msg := 'Address line 1  cannot be null';
+            RETURN;
+        END IF;
+        IF p_address_line2 IS NULL THEN
+            p_msg := 'Address line 2 cannot be null';
+            RETURN;
+        END IF;
+        IF p_city IS NULL THEN
+            p_msg := 'City cannot be null';
+            RETURN;
+        END IF;
+        IF p_state IS NULL THEN
+            p_msg := 'State cannot be null';
+            RETURN;
+        END IF;
+        IF p_country IS NULL THEN
+            p_msg := 'Country cannot be null';
+            RETURN;
+        END IF;
+        IF p_zipcode IS NULL THEN
+            p_msg := 'Zipcode cannot be null';
+            RETURN;
+        END IF;
+        
+        
+        -- Generate new user ID if not provided
+        IF p_user_id IS NULL THEN
+            SELECT MAX(UserID) + 1 INTO v_user_id FROM USERS;
+        ELSE
+            v_user_id := p_user_id;
+        END IF;
+
+        -- Insert new user and contact information
+        INSERT INTO USERS (UserID, RoleID, EmailID, UserName, Password, FirstName, LastName, Speciality, Nationality)
+        VALUES (users_seq.NEXTVAL, p_role_id, p_email_id, p_user_name, p_password, p_first_name, p_last_name, p_speciality, p_nationality);
+
+        INSERT INTO CONTACT (ContactID, UserID, ContactNumber, AddressLine1, AddressLine2, City, State, Country, ZipCode)
+        VALUES (contact_seq.NEXTVAL, users_seq.CURRVAL, p_contact_number, p_address_line1, p_address_line2, p_city, p_state, p_country, p_zipcode);
+
+        p_msg := 'User added successfully';
+        
+    ELSIF (p_action = 'UPDATE') THEN
+        IF p_user_id IS NULL THEN
+            SELECT UserID INTO a_user_id FROM USERS WHERE EmailID = p_email_id;
+            
+        END IF;
+        IF p_contact_id IS NOT NULL THEN
+            p_msg := 'Contact id should be null for update action';
+            RETURN;
+        END IF;
+       
+        -- Update user and contact information
+        UPDATE USERS SET RoleID = p_role_id, UserName = p_user_name, Password = p_password, FirstName = p_first_name, LastName = p_last_name, Speciality = p_speciality, Nationality = p_nationality
+        WHERE UserID = a_user_id and  EmailID = p_email_id;
+
+        UPDATE CONTACT SET ContactID = contact_seq.NEXTVAL, ContactNumber = p_contact_number, AddressLine1 = p_address_line1, AddressLine2 = p_address_line2, City = p_city, State = p_state, Country = p_country, ZipCode = p_zipcode
+        WHERE UserID = a_user_id;
+
+        p_msg := 'User updated successfully';
+ELSIF (p_action = 'DELETE') THEN
+    IF p_user_id IS NULL THEN
+            p_msg := 'User id cannot be null';
+            RETURN;
+        END IF;
+    
+    DELETE FROM CONTACT WHERE UserID = p_user_id;
+    DELETE FROM USERS WHERE UserID = p_user_id;
+    p_msg := 'User deleted successfully';
+ELSE
+    p_msg := 'Invalid action';
+END IF;
+END;
+/
+
--- PROJECT4/TESTCASES.sql
+++ PROJECT4/TESTCASES.sql
@@ -1,83 +1,122 @@
---TESTCASES FOR FUNCTION TO MANAGE USERS
---test case 1: Add a new user
+--TESTCASES FOR PROCEDURE MANAGE USERS
+-- Test case 1: Adding a new user
 DECLARE
-    v_result VARCHAR2(200);
+    v_msg VARCHAR2(100);
 BEGIN
-    v_result := manage_users(
+    manage_users(
+        p_user_id => NULL,
         p_role_id => 1,
-        p_email_id => 'testuser@example.com',
-        p_username => 'testuser',
-        p_password => 'password',
-        p_firstname => 'Test',
-        p_lastname => 'User',
-        p_speciality => 'Painting',
-        p_nationality => 'US',
-        p_contact_number => '1234567890',
-        p_address_line1 => '123 Main St',
-        p_address_line2 => 'Apt 5009',
+        p_email_id => 'johndoe@example.com',
+        p_user_name => 'johndoe',
+        p_password => 'password123',
+        p_first_name => 'John',
+        p_last_name => 'Doe',
+        p_speciality => 'Web Development',
+        p_nationality => 'USA',
+        p_contact_id => NULL,
+        p_contact_number => '5501751234',
+        p_address_line1 => '123 Main St.',
+        p_address_line2 => 'Apt. 4B',
         p_city => 'Anytown',
         p_state => 'CA',
         p_country => 'USA',
-        p_zipcode => 12345,
-        p_action => 'ADD'
+        p_zipcode => '12345',
+        p_action => 'ADD',
+        p_msg => v_msg
     );
-    DBMS_OUTPUT.PUT_LINE(v_result);
+    DBMS_OUTPUT.PUT_LINE(v_msg);
 END;
 /
--- Expected output: User added successfully
 
---test case 2: Update an existing user
+
+-- Test case 2: Adding a new user
 DECLARE
-    v_result VARCHAR2(200);
+    v_msg VARCHAR2(100);
 BEGIN
-    v_result := manage_users(
-        p_user_id => 31,
-        p_role_id => 2,
-        p_email_id => 'updateduser@example.com',
-        p_username => 'updateduser',
-        p_password => 'newpassword',
-        p_firstname => 'Updated',
-        p_lastname => 'User',
-        p_speciality => 'Sculpture',
-        p_nationality => 'CA',
-        p_contact_number => '0987654321',
-        p_address_line1 => '456 Elm St',
-        p_address_line2 => 'Apt 5009',
-        p_city => 'Othertown',
-        p_state => 'NY',
+    manage_users(
+        p_user_id => NULL,
+        p_role_id => 3,
+        p_email_id => 'testuser@example.com',
+        p_user_name => 'testuser',
+        p_password => 'password123',
+        p_first_name => 'test',
+        p_last_name => 'user',
+        p_speciality => NULL,
+        p_nationality => NULL,
+        p_contact_id => NULL,
+        p_contact_number => '5757979234',
+        p_address_line1 => '123 Main St.',
+        p_address_line2 => 'Apt. 4B',
+        p_city => 'testtown',
+        p_state => 'CA',
         p_country => 'USA',
-        p_zipcode => 54321,
-        p_action => 'UPDATE'
+        p_zipcode => '12345',
+        p_action => 'ADD',
+        p_msg => v_msg
     );
-    DBMS_OUTPUT.PUT_LINE(v_result);
+    DBMS_OUTPUT.PUT_LINE(v_msg);
 END;
 /
--- Expected output: User updated successfully
 
-
---test case 3: Delete a user
+--test case 3: Update an existing user
 DECLARE
-  v_msg VARCHAR2(200);
+    v_msg VARCHAR2(100);
 BEGIN
-  -- delete contact records first
-  DELETE FROM CONTACT WHERE UserID = 31;
-  
-  -- then delete user record
-  v_msg := manage_users(31, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'DELETE');
-  DBMS_OUTPUT.PUT_LINE(v_msg);
+    manage_users(
+        p_user_id => NULL,
+        p_role_id => 2,
+        p_email_id => 'johndoe@example.com',
+        p_user_name => 'johndoebbb',
+        p_password => 'password123',
+        p_first_name => 'John',
+        p_last_name => 'Doe',
+        p_speciality => 'Web Development',
+        p_nationality => 'USA',
+        p_contact_id => NULL,
+        p_contact_number => '5501751234',
+        p_address_line1 => '123 Main St.',
+        p_address_line2 => 'Apt. 4B',
+        p_city => 'New Anytown',
+        p_state => 'CA',
+        p_country => 'USA',
+        p_zipcode => '12345',
+        p_action => 'UPDATE',
+        p_msg => v_msg
+    );
+    DBMS_OUTPUT.PUT_LINE(v_msg);
 END;
 /
--- Expected output: User deleted successfully
 
---test case 4: Invalid action specified
+
+--test case 4: Delete a user
 DECLARE
-    v_result VARCHAR2(200);
+    v_msg VARCHAR2(100);
 BEGIN
-    v_result := manage_users(31, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'INVALID');
-    DBMS_OUTPUT.PUT_LINE(v_result);
+    manage_users(
+        p_user_id => 33,
+        p_role_id => NULL,
+        p_email_id => NULL,
+        p_user_name => NULL,
+        p_password => NULL,
+        p_first_name => NULL,
+        p_last_name => NULL,
+        p_speciality => NULL,
+        p_nationality => NULL,
+        p_contact_id => NULL,
+        p_contact_number => NULL,
+        p_address_line1 => NULL,
+        p_address_line2 => NULL,
+        p_city => NULL,
+        p_state => NULL,
+        p_country => NULL,
+        p_zipcode => NULL,
+        p_action => 'DELETE',
+        p_msg => v_msg
+    );
+    DBMS_OUTPUT.PUT_LINE(v_msg);
 END;
 /
--- Expected output: Invalid action
+
 
 --TESTCASES FOR FUNCTION TO PURCHASE ARTWORK AND UPDATED ARTWORK STATUS
 --testcase1: artworkid 3 status is available
